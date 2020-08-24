c4fd95f9a957dfa6d0f26c32e10e386ad704e4fb
Harsh Lodhi <harshlodhi82@gmail.com>
2020-08-24 18:34:43 +0530

    Added task 4

---
+++ Task_4/index.js
@@ -0,0 +1,22 @@
+const popUpBgElement = document.getElementById("pop_up_bg");
+const popUpElement = document.getElementById("popup");
+const cancelBtn = document.getElementById("popup_cancel_btn");
+const button = document.getElementById("click_me_btn");
+let isPopUpShowing = false;
+
+const popUpHandeler = ()=>{
+    isPopUpShowing = !isPopUpShowing;
+    if(isPopUpShowing){
+        popUpBgElement.classList.remove("hidden");
+        popUpElement.classList.remove("hidden");
+        cancelBtn.classList.remove("hidden");
+    }else{
+        popUpBgElement.classList.add("hidden");
+        popUpElement.classList.add("hidden");
+        cancelBtn.classList.add("hidden");
+    }
+}
+
+//** Events */
+button.addEventListener('click', popUpHandeler);
+cancelBtn.addEventListener('click', popUpHandeler);
